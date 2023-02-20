2719b1400b3066011ca27e74135dafb2fdf42a4f
Yogesh-heliverse <yogesh@heliverse.com>
2023-02-20 20:25:53 +0530

    modals

---
+++ src/api/career-single/controllers/career-single.ts
@@ -0,0 +1,7 @@
+/**
+ * career-single controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::career-single.career-single');
---
+++ src/api/career-single/routes/career-single.ts
@@ -0,0 +1,7 @@
+/**
+ * career-single router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::career-single.career-single');
